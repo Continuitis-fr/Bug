914512a4cc9f094c3cf3056f1464076d2ed5b158
Continuitis <morgan.conti@continuitis.com>
2021-10-05 21:54:20 +0200

    bug on generics

---
+++ ClassLibrary/Classes.cs
@@ -0,0 +1,30 @@
+﻿namespace ClassLibrary
+{
+    public class MyDtoItem : DtoItem
+    {
+
+    }
+
+    public class MyRepo : IRepository<MyDtoItem> 
+    {
+        public Task CreateAsync(MyDtoItem element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditAsync(MyDtoItem element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MyDtoItem?> GetAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<MyDtoItem>?> GetElementsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ClassLibrary/IRepository.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ClassLibrary
+{
+    public interface IRepository<TDtoItem> where TDtoItem : DtoItem
+    {
+        Task<IEnumerable<TDtoItem>?> GetElementsAsync();
+        Task CreateAsync(TDtoItem element);
+        Task<TDtoItem?> GetAsync(Guid id);
+        Task EditAsync(TDtoItem element);
+    }
+}
